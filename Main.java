874dde9b826b32af4a538b6c023c9b580ce47d5a
Aviraj lavate <101280208+aviraj00@users.noreply.github.com>
2025-04-06 18:25:01 +0530

    new commit

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.sql.Date;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("This is a test.");
+        System.out.println("This is a Jenkens.");
         System.out.println(new Date(System.currentTimeMillis()));
     }
 }
