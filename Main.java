9f4754a8076fae1519a61511483962ad81a93a15
Aviraj lavate <101280208+aviraj00@users.noreply.github.com>
2025-04-06 18:49:04 +0530

    2comiit
    hi

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.sql.Date;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello, Aviraj!");
-        System.out.println("This is a Jenkens.");
+        System.out.println("This is a j.");
         System.out.println(new Date(System.currentTimeMillis()));
     }
 }
