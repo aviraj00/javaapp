0d993bf264fa3161c98aa7a2621bc5fd22e32f9b
Aviraj lavate <101280208+aviraj00@users.noreply.github.com>
2025-04-06 18:23:00 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.sql.Date;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test.");
+        System.out.println(new Date(System.currentTimeMillis()));
+    }
+}
